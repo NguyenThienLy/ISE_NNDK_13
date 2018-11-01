58d38c0004c0bd5ef7b714e94abe8afc0b9ba93e
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2018-11-01 10:23:08 +0700

    update new project

---
+++ CanTeenManagement/CanTeenManagement/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CanTeenManagement.View;
+
+namespace CanTeenManagement.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        bool isLoaded = false;
+        // mọi thứ xử lý sẽ nằm trong này
+        public MainViewModel()
+        {
+            //if (!this.isLoaded)
+            //{
+            //    this.isLoaded = true;
+            //    LoginView lgV = new LoginView();
+
+            //    lgV.ShowDialog();
+            //}
+        }
+    }
+}
